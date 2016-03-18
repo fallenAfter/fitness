ce4e5773ea5d3f133416d93c6abaa6b3da3d4c87
fallenAfter <brandon@Brandons-MacBook-Pro.local>
2016-03-18 14:55:10 -0400

    added model for creating users to app

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports={
+	'url': 'mongodb://brandon:12345@ds023098.mlab.com:23098/fitness'
+}
---
+++ models/account.js
@@ -0,0 +1,17 @@
+// attach mongoose
+var mongoose= require('mongoose');
+var schema= mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+//create account schema
+var Account = new schema({
+	username: String,
+	password: String,
+	name: String,
+	someID: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+//make public
+module.exports= mongoose.model('Account', Account);
